cae47fb221f0d53494d4488a649abf26557276ec
Kashvirita <143584197+Kashvirita@users.noreply.github.com>
2024-06-15 13:23:44 +0000

    back to normal

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -45,8 +45,8 @@ const projectList = [
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and Tech abilities.",
+    url: "https://github.com/Kashvirita/Resume-",
   },
   {
     title: "AI blog website using only frontend framework",
